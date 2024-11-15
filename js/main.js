b63ae256e6feb0a37d8a745b78c5d0d9abfee45e
chrisna <chrisna.mitt@gmail.com>
2024-11-15 18:58:09 +0100

    test 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-alert('dfzfz')
+let motsATrouver =["voiture", "maison", "horloge", "piscine", "boulangerie"]
+
+let lettres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+const btnMots = document.querySelector(".boutonMot")
+
+btnMots.addEventListener("click", ()  => {
+
+
+})
+
+document.getElementById("boutonmot").innerHTML =
+    Math.floor(Math.random() * 10);
