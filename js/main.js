09d8bc313a22da412c8224efbbfd298dd83bdc52
chrisna <chrisna.mitt@gmail.com>
2024-11-17 16:55:54 +0100

    on peut choisir un mot aleatoire et ca remplace les lettres par des "_"

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-let motsATrouver =["voiture", "maison", "horloge", "piscine", "boulangerie"]
-
-let lettres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-const btnMots = document.querySelector(".boutonMot")
-
-btnMots.addEventListener("click", ()  => {
+//mots a trouver
+let mots =["car", "jeux", "horloge", "piscine", "boulangerie"];
+let lettres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 
-})
+//permet de choisir un mot aléatoire parmis les mots du tableau
+let motAleatoire = mots[Math.floor(Math.random() * mots.length)];
+console.log("voici le mot", motAleatoire)
 
-document.getElementById("boutonmot").innerHTML =
-    Math.floor(Math.random() * 10);
+
+//Array creer un tableau de la meme valeur que le mot choisis mais qui est remplacer par des tiret
+let motCacher = Array(motAleatoire.length).fill("_");
+
+//.join permet de mettre des espaces
+console.log("devine le mot",motCacher.join(" "));
+
